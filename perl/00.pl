9c2a72212eb58c14bfac9651eea3230d85c096c7
Teo Wei Shen <tws4793@gmail.com>
2021-03-23 16:21:00 +0800

    Begin perl crash course with hello world

---
+++ perl/00.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+=begin comment
+`print` can be with or without parantheses
+Can also include white space as well
+Usually have `;` for closing, but optional
+=cut
+
+# Hello world
+print "Hello world!\n";
+print("Hello world!\n");
+print    "Hello world!\n";
+print "Hello
+        world\n";
